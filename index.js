47910928b57422336bcb1cc682302313404cedab
addr_18 <anggydahianaruiz99@gmail.com>
2021-08-24 11:46:41 -0500

    make it better

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const bodyparser = require('body-parser')
 require('dotenv').config();
 const app = express();
 
+
+// cors
+const cors = require('cors');
+var corsOptions = {
+    origin: '*', // Reemplazar con dominio
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
+
+
 // capturar body 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
