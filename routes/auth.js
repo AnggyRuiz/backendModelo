43e1062b09604fb572936785c1656a19fccdf6c7
addr_18 <anggydahianaruiz99@gmail.com>
2021-09-03 15:02:36 -0500

    query num

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const schemaRegister = Joi.object({
     name: Joi.string().min(4).max(255).required(),
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required(),
-    queryNum: Joi.string().min(6).max(1024).required()
+    queryNum: Joi.string().min(1).required()
 })
 const schemaLogin = Joi.object({
     email: Joi.string().min(6).max(255).required().email(),
