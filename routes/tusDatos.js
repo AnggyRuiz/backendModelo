73e0ef575df374f0f7dcc2cab433dfe2b255e6ca
addr_18 <anggydahianaruiz99@gmail.com>
2021-09-23 16:35:35 -0500

    pruebas pdf

--- routes/tusDatos.js
+++ routes/tusDatos.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const request = require('request');
 var mongoose = require('mongoose');
-var fs = require('fs');
+var bodyParser = require('body-parser')
+router.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+var rawParser = bodyParser.raw()
+var axios = require('axios');
+var fs = require('fs')
 
 //PROD
 const username = 'sosorno@isciolab.com';
@@ -48,27 +52,58 @@ router.post('/launch', (req, res) => {
         console.log(response.body)
     })
 })
-router.post('/report', (req, res) => {
+router.post('/report', rawParser, (req, res) => {
     const id = req.body.id;
     console.log(id);
     var options = {
         'method': 'GET',
-        'url': `  https://dash-board.tusdatos.co/api/report_pdf/${id}`,
+        'url': `https://dash-board.tusdatos.co/api/report_pdf/${id}`,
         'headers': {
             'Authorization': idToken,
+            'Content-Type': 'application/pdf'
         },
     };
-    request(options, (error, response) => {
+    request(options, function(error, resp) {
         if (error) throw new Error(error);
-        res.send(response)
-        console.log(response);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'filename=tusdatos-1032456318-15-09-2021.pdf')
+        res.setHeader('Content-Description', 'File Transfer')
+            /* var stat = fs.statSync(resp);
+            console.log(stat); */
+        res.send(resp)
+            //file.pipe(res);
+            /*  var file = fs.createReadStream(resp);
+             res.setHeader('Content-Length', stat.size);
+             res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+              */
     });
+
+
+
+    /* var config = {
+        method: 'get',
+        url: 'https://dash-board.tusdatos.co/api/report_pdf/61421ff79368cc94120e0fc1',
+        headers: {
+            'Authorization': 'Basic c29zb3Jub0Bpc2Npb2xhYi5jb206VGVsbW8yMDIx'
+        }
+    };
+ 
+    axios(config)
+        .then(function(response) {
+            console.log(JSON.stringify(response.data));
+            res.send(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+ */
 
 })
 router.post('/result', (req, res) => {
     console.log(req.body.jobkey);
     const id = req.body.jobkey;
     console.log(id);
+
     var options = {
         'method': 'GET',
         'url': `  https://dash-board.tusdatos.co/api/results/${id}`,
